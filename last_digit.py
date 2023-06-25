09a1d14f126a6b8702c176ddd33dfca5d27fa800
Yogesh More <morey@oldwestbury.edu>
2023-06-25 11:25:15 -0600

    spacings histogram

--- last_digit.py
+++ last_digit.py
@@ -6,7 +6,7 @@ import matplotlib
 #%%
 datafile='zeta_zeros10e5'
 
-df=pd.read_csv(datafile, header=None)
+df=pd.read_csv(datafile, header=None, names=['zeroes'])
 
 # %%
 print(df.head())
@@ -15,7 +15,7 @@ with open(datafile, 'r') as my_file:
     print(my_file.readlines())
 
 #%%
-df['round']=np.floor(df[[0]])%10
+df['round']=np.floor(df['zeroes'])%10
 df.hist('round', bins=10)
 
 
@@ -25,8 +25,15 @@ directory='./histogram'
 os.mkdir(directory)
 
 #%%
-df['fractional']=np.modf(df[[0]])[0]
+df['fractional']=np.modf(df['zeroes'])[0]
 hist_fractional=df.hist('fractional', bins=10)
 
 hist_fractional[0][0].figure.savefig('histogram/fractional_parts')
+
+# %%
+df['spacings']=df['zeroes'].diff()
+
+# %%
+hist_spacings=df.hist('spacings', bins=100)
+
 # %%
